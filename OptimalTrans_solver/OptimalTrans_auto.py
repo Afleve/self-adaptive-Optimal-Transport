4aeb7a4b08de57f45cc8f49c33bf15ced2ebb306
Afleve <graceafleve@gmail.com>
2026-05-18 13:42:50 +0800

    update inductive from Afleve Grace

--- OptimalTrans_solver/OptimalTrans_auto.py
+++ OptimalTrans_solver/OptimalTrans_auto.py
@@ -1,31 +1,66 @@
 from .OptimalTrans_utils import *
 import time
+import torch
 import numpy as np
 
+def evalute_test(cfg, test_features, test_labels, y_hats, gmm_adapter_list, alpha, beta):
+    num_samples = test_features[0].shape[0]
+    K = y_hats[0].shape[1]
+    y_hats = [y_hat.to(cfg['device']) for y_hat in y_hats]
+    test_features = [features.to(cfg['device']) for features in test_features] 
+    test_labels = test_labels.to(cfg['device'])
 
-def OptimalTrans(vlm_features, vfm_features, true_labels=None, y_hats=None, cfg=None):
+    y_hats, z = init_z(y_hats, test_features, 0.01, 15, cfg, test_labels, initialize='avg')
+
+
+    gmm_likelihood_list = []
+    for i, adapter in enumerate(gmm_adapter_list):
+        gmm_likelihood = adapter(test_features[i], no_exp=True)
+        intermediate_temp = gmm_likelihood.clone()
+        intermediate_temp -= torch.max(intermediate_temp, dim=1, keepdim=True)[0]
+        intermediate_temp = torch.exp(1 / 50 * intermediate_temp)
+        intermediate_temp = intermediate_temp / torch.sum(intermediate_temp, dim=1, keepdim=True)
+        gmm_likelihood_list.append(intermediate_temp)
+    fusion = torch.zeros(num_samples, K).to(cfg['device'])
+    for i, P in enumerate(gmm_likelihood_list):
+        fusion += P * alpha[i]
+    for i, Y in enumerate(y_hats):
+        fusion += Y * beta[i]
+    acc = cls_acc(fusion, test_labels)
+    print(f"**** Inductive Accuracy: {acc:.2f} ****")
+
+
+
+def OptimalTrans(all_features, true_labels=None, y_hats=None, cfg=None):
     
     if cfg is not None:
         import os
         os.makedirs(f'{cfg["root"]}', exist_ok=True)
     else:
         cfg = {
-            'device' : 'cuda'
+            'device' : 'cuda',
+            'setting' : 'Transductive'
         }
+
+    torch.cuda.synchronize()
+    mem_before = torch.cuda.memory_reserved() / 1024**2
+    start_time = time.time()
 
     y_hats = [y_hat.to(cfg['device']) for y_hat in y_hats]
     if isinstance(true_labels, torch.Tensor):
         true_labels = true_labels.to(cfg['device'])
     K = y_hats[0].shape[1]
     num_samples =  y_hats[0].shape[0]
-    features_list = [features.to(cfg['device']) for features in [*vlm_features, *vfm_features]] 
-    # features_list = torch.cat(features_list, dim=-1) 
-    # features_list = [torch.nn.functional.normalize(features_list, dim=-1)]
+    features_list = [features.to(cfg['device']).float() for features in all_features] 
+    for features in features_list:
+        print("features.shape = ", features.shape)
+
+    print("K = ", K , "num_samples = ", num_samples)
 
     iterations_to_try = list(range(0, 10))
 
-    eps = 0.01
-    S_iters = 10
+    eps = cfg.get('eps') if cfg.get('eps') else 0.01
+    S_iters = 15
 
     y_hats, z = init_z(y_hats, features_list, eps, S_iters, cfg, true_labels, initialize='avg')
     z_clone = z.clone()
@@ -40,18 +75,17 @@ def OptimalTrans(vlm_features, vfm_features, true_labels=None, y_hats=None, cfg=
             adapter = Gaussian(mu=mu, std=std).to(cfg['device'])
             gmm_adapter_list[i] = adapter
         gmm_likelihood_list = [None] * len(features_list)
-        alpha = torch.ones(len(gmm_adapter_list)).to(cfg['device']) / (len(gmm_adapter_list))
-        beta = torch.ones(len(y_hats)).to(cfg['device']) / (len(y_hats))
+        alpha = torch.ones(len(gmm_adapter_list)).to(cfg['device']) / len(gmm_adapter_list)
+        beta = torch.ones(len(y_hats)).to(cfg['device']) / len(y_hats)
         
         for k in iterations_to_try:
             for i, adapter in enumerate(gmm_adapter_list):
                 gmm_likelihood = adapter(features_list[i], no_exp=True)
                 intermediate_temp = gmm_likelihood.clone()
                 intermediate_temp -= torch.max(intermediate_temp, dim=1, keepdim=True)[0]
-                intermediate_temp = torch.exp(1 / 10 * intermediate_temp)
+                intermediate_temp = torch.exp(1 / 50 * intermediate_temp)
                 intermediate_temp = intermediate_temp / torch.sum(intermediate_temp, dim=1, keepdim=True)
                 gmm_likelihood_list[i] = intermediate_temp
-
 
             fusion = torch.zeros(num_samples, K).to(cfg['device'])
             for i, P in enumerate(gmm_likelihood_list):
@@ -60,24 +94,32 @@ def OptimalTrans(vlm_features, vfm_features, true_labels=None, y_hats=None, cfg=
                 fusion += Y * beta[i]
 
             z = sinkhorn(fusion, eps, S_iters)
+            
+            if not len(gmm_likelihood_list) and len(y_hats)==1:
+                break
 
             if len(gmm_adapter_list) > 1:
                 alpha = update_alpha_per_source(z, gmm_likelihood_list)
             if len(y_hats) > 1:
                 beta = update_alpha_per_source(z, y_hats)
-                
             
             for i, adapter in enumerate(gmm_adapter_list):
                 adapter = update_mu(adapter, features_list[i], z)
                 adapter = update_sigma(adapter, features_list[i], z)
                 gmm_adapter_list[i] = adapter
+            acc = cls_acc(z, true_labels)
 
         if isinstance(true_labels, torch.Tensor):
             acc = cls_acc(z, true_labels)
-        # print(alpha)
-        # print(beta)
         
-        # with open(f'{cfg["root"]}/{cfg["dataset"]}_log.txt', "a") as f: f.write(f"**** eps={eps:.4f}, it={S_iters:02d}, lambda={lambda_value:.4f}, k={k:02d} accuracy: {acc:.2f} ****\n")
         print(f"**** eps={eps:.4f}, it={S_iters:02d}, k={k:02d} accuracy: {acc:.2f} ****")
+    
+    print("T token time: ", time.time() - start_time)
+    torch.cuda.synchronize()
+    mem_after = torch.cuda.memory_reserved() / 1024**2
+    print(f"GPU Memory: {mem_before:.2f} MB -> {mem_after:.2f} MB (delta: {mem_after - mem_before:.2f} MB)")
+    
+    if cfg['setting'] == 'Inductive':
+        return gmm_adapter_list, alpha, beta
 
-    return z
+    return z, y_hats
--- main.py
+++ main.py
@@ -2,34 +2,39 @@ import os
 import random
 import argparse
 import numpy as np
-from datasets import get_all_dataloaders
+from datasets import *
 from utils import *
-
+from OptimalTrans_solver.OptimalTrans_utils import prepare_objects
+from OptimalTrans_solver.OptimalTrans_auto import OptimalTrans, evalute_test
+from modelscope import AutoModel
 import warnings
+import time
+import torch
 
 warnings.filterwarnings("ignore")
 
 
 def get_arguments():
     parser = argparse.ArgumentParser()
-    parser.add_argument('--dataset', default='caltech101', help='dataset name', type=str)
+    parser.add_argument('--dataset', default='imagenet', help='dataset name', type=str)
     parser.add_argument('--root_path', default='/data/Public', type=str)
-    parser.add_argument('--method', default='OptimalTrans', type=str, help='')
-    parser.add_argument('--shots', default=0, type=int)
-    parser.add_argument('--seed', default=0, type=int)
+    parser.add_argument('--method', default='SelfApativeOptimalTrans', type=str,
+                        help='')
+    parser.add_argument('--unsup_shots', default=-1, type=int)
+    parser.add_argument('--seed', default=1, type=int)
+
     parser.add_argument('--backbone', default='vit_b16', type=str)
-    parser.add_argument('--visionLanguageM', default='clip', type=str)
-    parser.add_argument('--visionFMs', default=['dinov3', 'dinov2'], type=list)
+    parser.add_argument('--vlms', default='clip', type=str)
+    parser.add_argument('--vfms', default='dinov2,dinov3', type=str)
+    parser.add_argument('--device', default='cuda', type=str)
+        
+    parser.add_argument('--eps', default=0.01, type=float)
 
     parser.add_argument('--prototypes_path', default="./prototypes")
-    parser.add_argument('--setting', default="")
+    parser.add_argument('--setting', default="Transductive", choices=['Inductive', 'Transductive'])
     parser.add_argument('--prototypes_method', default="")
     parser.add_argument('--prototypes_dataset', default="")
     parser.add_argument('--prototypes_shots', default=16)
-
-    parser.add_argument('--root', default='temp', type=str)
-    parser.add_argument('--device', default='cuda', type=str)
-    parser.add_argument('--cat_bool', action='store_true')
 
     args = parser.parse_args()
 
@@ -44,29 +49,28 @@ def set_random_seed(seed):
 
 
 def main():
-    backbones = {'rn50': 'RN50',
-                 'rn101': 'RN101',
-                 'vit_b16': 'ViT-B/16',
-                 'vit_b32': 'ViT-B/32',
-                 'vit_l14': 'ViT-L/14'}
 
     # Load config file
     args = get_arguments()
 
     cfg = {'dataset': args.dataset}
-    cache_dir = os.path.join(f'./caches/{args.backbone}', cfg['dataset'])
+
+    cache_dir = os.path.join('./caches', cfg['dataset'])
     os.makedirs(cache_dir, exist_ok=True)
-    cfg['cache_dir'] = cache_dir
-    cfg['load_cache'] = True
-    cfg['load_pre_feat'] = True
+    cfg['cache_root'] = cache_dir
+    cfg['root'] = "records"
 
+    signal = True
+    cfg['load_cache'] = signal
+    cfg['load_pre_feat'] = signal
 
-    cfg['backbone'] = backbones[args.backbone]
+    print("\nRunning configs.")
+
 
     cfg['seed'] = args.seed
     cfg['root_path'] = args.root_path
     cfg['method'] = args.method
-    cfg['shots'] = args.shots
+    cfg['shots'] = args.unsup_shots
 
     cfg['prototypes_path'] = args.prototypes_path
     cfg['setting'] = args.setting
@@ -74,125 +78,81 @@ def main():
     cfg['prototypes_dataset'] = args.prototypes_dataset
     cfg['prototypes_shots'] = args.prototypes_shots
 
-    cfg['root'] = args.root
-    cfg['cat_bool'] = args.cat_bool
+    cfg['vlms'] = args.vlms.split(',')
+    cfg['backbone'] = args.backbone.split(',')
+    cfg['vfms'] = args.vfms.split(',')
     cfg['device'] = args.device
-    cfg['backbone_name'] = args.backbone
 
-    # CLIP
-    clip_model, preprocess = clip.load(cfg['backbone'])
-    clip_model.eval()
+    print(cfg, "\n")
 
     # Prepare dataset
     set_random_seed(args.seed)
 
-    train_loader, val_loader, test_loader, dataset = get_all_dataloaders(cfg, preprocess)
+    print("Preparing dataset.")
 
-    _, _, _, _, vlm_feature, true_labels, clip_prototypes = get_all_features(
-        cfg, train_loader, val_loader, test_loader, dataset, clip_model, args.visionLanguageM)
-    clip_model = clip_model.to('cpu')  # unload CLIP model from VRAM
+    dataset = get_all_datasets(cfg, preprocess=None)
 
-    vlm_features = [vlm_feature.float()]
-    # visionFM
-    vfm_features = []
-    for visionFM in args.visionFMs:
-        if visionFM == 'dinov2':
-            import models.dinov2.visiontransformer as vits
-            pretrain_path = 'yours/path/to/dinov2_vitb16_pretrain_lvd1689m-73cec8be.pth'
-            dinov2_model = vits.__dict__['vit_base']().cuda()
-            torch.cuda.empty_cache()
-            state_dict = torch.load(pretrain_path, map_location='cuda')
-            dinov2_model.load_state_dict(state_dict)
-            dinov2_model.eval()
-            vfm_feature, _ = pre_load_features(cfg, "test", dinov2_model, test_loader, visionLanguageM='dinov2')
-        if visionFM == "dinov3":
-            url = "yours/.cache/torch/hub/checkpoints/dinov3_vitb16_pretrain_lvd1689m-73cec8be.pth"
-            dinov3_model = torch.hub.load('yours/dinov3/', 'dinov3_vitb16', source='local', weights=url).cuda()
-            dinov3_model.eval()
-            vfm_feature, _ = pre_load_features(cfg, "test", dinov3_model, test_loader, visionLanguageM='dinov3')
+    print("Loading features.")
 
-        vfm_features.append(vfm_feature.cuda())
+    vlms_test_features, vlms_train_features, vlms_test_probs, vlms_train_probs = [], [], [], []
+    for vlm in cfg['vlms']:
+        if vlm == 'clip':
+            for backbone in cfg['backbone']:
+                cfg['cache_dir'] = os.path.join(cfg['cache_root'], vlm, backbone)
+                os.makedirs(cfg['cache_dir'], exist_ok=True)
 
-    if args.method == 'OptimalTrans':
-        from OptimalTrans_solver.OptimalTrans_utils import prepare_objects
-        y_hat = prepare_objects(vlm_feature, true_labels, clip_prototypes)
-        from OptimalTrans_solver.OptimalTrans_auto import OptimalTrans
-        OptimalTrans(vlm_features, vfm_features, true_labels, [y_hat], cfg)
-    # elif args.method == 'TransCLIP':
-    #     from TransCLIP_solver.TransCLIP import TransCLIP_solver
-    #     if cfg['shots'] == 0:
-    #         TransCLIP_solver(shot_features, shot_labels, val_features, val_labels, test_features, test_labels,
-    #                          clip_prototypes)
-    #     else:
-    #         shot_features = shot_features.unsqueeze(0)
-    #         TransCLIP_solver(shot_features, shot_labels, val_features, val_labels, test_features, test_labels,
-    #                          clip_prototypes)
+                clip_model, preprocess = clip.load(backbones[backbone])
+                clip_model.eval()
+                train_loader, test_loader = dataset_2_dataloader(cfg, dataset, preprocess)
+                
+                shot_features, shot_labels, val_features, val_labels, test_features, test_labels, clip_prototypes = get_all_features(
+                    cfg, train_loader, None, test_loader, dataset, clip_model)
+                clip_model = clip_model.to('cpu')
 
-    # elif args.method == 'On-top':
+                vlms_test_features.append(test_features)
+                test_probs = prepare_objects(test_features, test_labels, clip_prototypes)
+                vlms_test_probs.append(test_probs)
 
-    #     from TransCLIP_solver.TransCLIP import TransCLIP_solver
-    #     from TransCLIP_solver.bench_utils import prepare_for_bench
+                torch.save(clip_prototypes, os.path.join(cfg['cache_dir'], 'clip_prototypes.pth'))
 
-    #     test_features, test_labels, initial_prototypes, initial_logits, clip_prototypes = prepare_for_bench(args,
-    #                                                                                                         test_features,
-    #                                                                                                         test_labels,
-    #                                                                                                         clip_prototypes)
+                if cfg['setting'] == 'Inductive':
+                    vlms_train_features.append(shot_features)
+                    train_probs = prepare_objects(shot_features, shot_labels, clip_prototypes)
+                    vlms_train_probs.append(train_probs)
 
-    #     if cfg['shots'] == 0:
-    #         TransCLIP_solver(shot_features, shot_labels, val_features, val_labels, test_features, test_labels,
-    #                          clip_prototypes, initial_prototypes, initial_logits)
-    #     else:
-    #         shot_features = shot_features.unsqueeze(0)
-    #         TransCLIP_solver(shot_features, shot_labels, val_features, val_labels, test_features, test_labels,
-    #                          clip_prototypes, initial_prototypes, initial_logits)
+    vfms_test_features, vfms_train_features = [], []
+    for vfm in cfg['vfms']:
+        vfm_model = None
+        if vfm == '':
+            break
+        if vfm == 'dinov2':
+            if not cfg['load_cache'] or not cfg['load_pre_feat']:
+                vfm_model = AutoModel.from_pretrained('facebook/dinov2-with-registers-large')
+            preprocess = transform_v2
+        if vfm == "dinov3":
+            if not cfg['load_cache'] or not cfg['load_pre_feat']:
+                vfm_model = AutoModel.from_pretrained("facebook/dinov3-vitl16-pretrain-lvd1689m")
+            preprocess = transform_v3
+        cfg['cache_dir'] = os.path.join(cfg['cache_root'], vfm)
+        os.makedirs(cfg['cache_dir'], exist_ok=True)
+        if not cfg['load_cache'] or not cfg['load_pre_feat']:
+            vfm_model = vfm_model.to(cfg['device'])
+            vfm_model.eval()
+        train_loader, test_loader = dataset_2_dataloader(cfg, dataset, preprocess)
+        test_features, _ = pre_load_features(cfg, "test", vfm_model, test_loader, modelName=vfm)
+        vfms_test_features.append(test_features)
+        if cfg['setting']=="Inductive":
+                train_features, _ = build_cache_model(cfg, vfm_model, train_loader, n_views=1, modelName=vfm)
+                vfms_train_features.append(train_features.to(cfg['device']))
 
+    if cfg['setting'] == 'Transductive':
+        start_time = time.time()
+        z, y_hats = OptimalTrans([*vlms_test_features, *vfms_test_features], test_labels, vlms_test_probs, cfg)
+        print("Cost time:", time.time() - start_time)
+    else:
+        gmm_adapter_list, alpha, beta = OptimalTrans([*vlms_train_features, *vfms_train_features], shot_labels, vlms_train_probs, cfg)
+        evalute_test(cfg, [*vlms_test_features, *vfms_test_features], test_labels, vlms_test_probs, gmm_adapter_list, alpha, beta)
 
-    # elif args.method == 'transductive_finetuning':
-    #     device = clip_prototypes.device
-    #     shot_features = shot_features.type(torch.FloatTensor).squeeze().to(device)
-    #     val_features = val_features.type(torch.FloatTensor).squeeze().to(device)
-    #     test_features = test_features.type(torch.FloatTensor).squeeze().to(device)
-    #     clip_prototypes = clip_prototypes.type(torch.FloatTensor).squeeze().to(device)
-    #     from baselines.runner import run_transductive_finetuning
-    #     run_transductive_finetuning(cfg, shot_features, shot_labels, val_features, val_labels, test_features,
-    #                                 test_labels,
-    #                                 clip_prototypes)
-    # elif args.method == 'bdcspn':
-    #     device = clip_prototypes.device
-    #     shot_features = shot_features.type(torch.FloatTensor).squeeze().to(device)
-    #     val_features = val_features.type(torch.FloatTensor).squeeze().to(device)
-    #     test_features = test_features.type(torch.FloatTensor).squeeze().to(device)
-    #     clip_prototypes = clip_prototypes.type(torch.FloatTensor).squeeze().to(device)
-    #     from baselines.runner import run_bdcspn
-    #     run_bdcspn(cfg, shot_features, shot_labels, val_features, val_labels, test_features, test_labels,
-    #                clip_prototypes)
-    # elif args.method == 'laplacian_shot':
-    #     device = clip_prototypes.device
-    #     shot_features = shot_features.type(torch.FloatTensor).squeeze().to(device)
-    #     val_features = val_features.type(torch.FloatTensor).squeeze().to(device)
-    #     test_features = test_features.type(torch.FloatTensor).squeeze().to(device)
-    #     clip_prototypes = clip_prototypes.type(torch.FloatTensor).squeeze().to(device)
-    #     from baselines.runner import run_laplacian_shot
-    #     run_laplacian_shot(cfg, shot_features, shot_labels, val_features, val_labels, test_features, test_labels,
-    #                        clip_prototypes)
-    # elif args.method == 'ptmap':
-    #     device = clip_prototypes.device
-    #     shot_features = shot_features.type(torch.FloatTensor).squeeze().to(device)
-    #     val_features = val_features.type(torch.FloatTensor).squeeze().to(device)
-    #     test_features = test_features.type(torch.FloatTensor).squeeze().to(device)
-    #     clip_prototypes = clip_prototypes.type(torch.FloatTensor).squeeze().to(device)
-    #     from baselines.runner import run_pt_map
-    #     run_pt_map(cfg, shot_features, shot_labels, val_features, val_labels, test_features, test_labels,
-    #                clip_prototypes)
-    # elif args.method == 'tim':
-    #     device = clip_prototypes.device
-    #     shot_features = shot_features.type(torch.FloatTensor).squeeze().to(device)
-    #     val_features = val_features.type(torch.FloatTensor).squeeze().to(device)
-    #     test_features = test_features.type(torch.FloatTensor).squeeze().to(device)
-    #     clip_prototypes = clip_prototypes.type(torch.FloatTensor).squeeze().to(device)
-    #     from baselines.runner import run_tim
-    #     run_tim(cfg, shot_features, shot_labels, val_features, val_labels, test_features, test_labels,
-    #             clip_prototypes, version='adm')
 
 
 if __name__ == '__main__':
